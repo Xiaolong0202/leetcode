6193c9778baf8314e3999bebbcd5cdfde242e76b
lxl <1343024614@qq.com>
2023-08-07 13:53:14 +0800

    309. 买卖股票的最佳时机含冷冻期

--- src/Solution.java
+++ src/Solution.java
@@ -1723,18 +1723,18 @@ public class Solution {
     /**
      * 123. 买卖股票的最佳时机 III
      */
-    public int maxProfit(int[] prices) {
-        if(prices.length<=1)return 0;
-        int buy1 = -prices[0], sell1 = 0;
-        int buy2 = -prices[0], sell2 = 0;
-        for (int i = 1; i < prices.length; i++) {
-            buy1 = Math.max(buy1,-prices[i]);
-            sell1 = Math.max(sell1,buy1+prices[i]);
-            buy2 = Math.max(buy2,sell1-prices[i]);
-            sell2 = Math.max(sell2,buy2+prices[i]);
-        }
-        return sell2;
-    }
+//    public int maxProfit(int[] prices) {
+//        if(prices.length<=1)return 0;
+//        int buy1 = -prices[0], sell1 = 0;
+//        int buy2 = -prices[0], sell2 = 0;
+//        for (int i = 1; i < prices.length; i++) {
+//            buy1 = Math.max(buy1,-prices[i]);
+//            sell1 = Math.max(sell1,buy1+prices[i]);
+//            buy2 = Math.max(buy2,sell1-prices[i]);
+//            sell2 = Math.max(sell2,buy2+prices[i]);
+//        }
+//        return sell2;
+//    }
 
     /**
      *188. 买卖股票的最佳时机 IV
@@ -1765,7 +1765,31 @@ public class Solution {
         }
         return dp[k*2];
     }
+
+    /**
+     * 309. 买卖股票的最佳时机含冷冻期
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int dpBuy[] = new int[prices.length];
+        int dpSell[] = new int[prices.length];
+        dpBuy[0] = -prices[0];
+        dpSell[0] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            dpBuy[i] = Math.max(dpBuy[i-1],dpSell[Math.max(i - 2, 0)]-prices[i]);
+            dpSell[i] = Math.max(dpSell[i-1],dpBuy[i]+prices[i]);
+        }
+        return dpSell[prices.length-1];
+    }
+
+
+
+
 }
+
+
+
 
 
 
