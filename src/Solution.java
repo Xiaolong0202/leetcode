f064228536e91710b122b43ec78e20a344e843ea
lxl <1343024614@qq.com>
2023-08-10 17:27:47 +0800

    1035. 不相交的线

--- src/Solution.java
+++ src/Solution.java
@@ -1849,6 +1849,26 @@ public class Solution {
         return dp[charArr1.length][charArr2.length];
     }
 
+    /**
+     * 1035. 不相交的线
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int [][] dp = new int[nums1.length+1][nums2.length+1];
+        for (int i = 1; i <= nums1.length ; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[nums1.length][nums2.length];
+    }
+
 }
 
 
