e00d9d2682f305fc31bbcb2f0956cefa669df077
liuxiaolong <1343024614@qq.com>
2023-09-02 19:27:44 +0800

    169. 多数元素 投票法求质数

--- src/Solution.java
+++ src/Solution.java
@@ -2224,6 +2224,23 @@ public class Solution {
         }
     }
 
+    /**
+     *169. 多数元素 投票法求质数
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums) {
+            if (count==0)candidate = num;
+            if (num!=candidate)count--;
+            else count++;
+        }
+        return candidate;
+    }
+
+
     public static void main(String[] args) {
     }
 
