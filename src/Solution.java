1879d9f35bd7e9770f327276fab540536b4e28d4
liuxiaolong <1343024614@qq.com>
2023-09-02 17:01:45 +0800

    142. 环形链表 II

--- src/Solution.java
+++ src/Solution.java
@@ -2172,6 +2172,31 @@ public class Solution {
         return a;
     }
 
+
+    /**
+     * 142. 环形链表 II
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (true) {
+            if (fast == null || fast.next == null)return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (fast!=slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
+
     public static void main(String[] args) {
     }
 
